f05147df9b9715ffb78ac2e4264190f4b5933ce1
DataWithAlex <explore@datawithalex.com>
2024-09-27 14:25:01 -0400

    add categories

--- app/webflow.py
+++ app/webflow.py
@@ -10,51 +10,11 @@ from typing import Dict, Optional
 from .logger_config import logger
 from .utils import categories, get_category_ids  # Updated import statement
 
-
-
 # Logging configuration
 logging.basicConfig(level=logging.INFO)
 
 # Define OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
-# Define the list of categories with their names and IDs
-categories = [
-    {"name": "Animals", "id": "668329ae71bf22a23a6ac94b"},
-    {"name": "International Relations", "id": "663299c73b94826974bd24da"},
-    {"name": "National Security", "id": "6632997a194f0d20b0d24108"},
-    {"name": "Civil Rights", "id": "663298e4562bd3696c89b3ea"},
-    {"name": "Arts", "id": "660ede71e88a45fcd08e2e39"},
-    {"name": "Energy", "id": "660ed44984debef46e8d5c5d"},
-    {"name": "Military and Veterans", "id": "65ce5778dae6450ac15a2d2f"},
-    {"name": "Priority Bill", "id": "65ba9dbe9768a6290a95c945"},
-    {"name": "Media", "id": "65b550562534316ee17131c0"},
-    {"name": "LGBT", "id": "655288ef928edb128306753e"},
-    {"name": "Public Records", "id": "655288ef928edb128306753d"},
-    {"name": "Social Welfare", "id": "655288ef928edb12830673e2"},
-    {"name": "Technology", "id": "655288ef928edb128306743e"},
-    {"name": "Government", "id": "655288ef928edb12830673e1"},
-    {"name": "Business", "id": "655288ef928edb128306746b"},
-    {"name": "Employment", "id": "655288ef928edb1283067425"},
-    {"name": "Public Safety", "id": "655288ef928edb1283067442"},
-    {"name": "Drugs", "id": "655288ef928edb128306745e"},
-    {"name": "Immigration", "id": "655288ef928edb12830673e5"},
-    {"name": "Transportation", "id": "655288ef928edb1283067415"},
-    {"name": "Criminal Justice", "id": "655288ef928edb12830673dc"},
-    {"name": "Elections", "id": "655288ef928edb12830673e0"},
-    {"name": "Culture", "id": "655288ef928edb1283067436"},
-    {"name": "Sports", "id": "655288ef928edb12830673df"},
-    {"name": "Marriage", "id": "655288ef928edb128306742d"},
-    {"name": "Housing", "id": "655288ef928edb128306743d"},
-    {"name": "Education", "id": "655288ef928edb12830673e4"},
-    {"name": "Medical", "id": "655288ef928edb12830673e9"},
-    {"name": "State Parks", "id": "655288ef928edb128306745d"},
-    {"name": "Guns", "id": "655288ef928edb128306741f"},
-    {"name": "Disney", "id": "655288ef928edb128306742c"},
-    {"name": "Natural Disasters", "id": "655288ef928edb1283067435"},
-    {"name": "Environment", "id": "655288ef928edb128306741b"},
-    {"name": "Taxes", "id": "655288ef928edb128306745c"}
-]
 
 # Function to get the top 3 categories using GPT-4o
 def get_top_categories(bill_text, categories, model="gpt-4o"):
